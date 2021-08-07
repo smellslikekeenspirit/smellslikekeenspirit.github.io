f8efd1dc5732667f15cd4a6558519f11b1d42e72
Prionti Nasir <t-pnasir@microsoft.com>
2021-08-07 15:41:22 -0400

    moved files to /docs

---
+++ docs/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import MainPage from './Card.js';
+
+function App() {
+  return (
+    <div class="blob-container">
+          <MainPage></MainPage>
+    </div>
+  );
+}
+
+export default App;
